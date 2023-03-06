2fce95327ffcfd919cc137702613ccd3fb185178
Anas010101 <127116276+Anas010101@users.noreply.github.com>
2023-03-06 23:57:10 +0530

    adding missing filters

--- src/rest.py
+++ src/rest.py
@@ -5,14 +5,24 @@ from sqlalchemy.orm import Session
 app = FastAPI(title="Weather App", debug=True)
 
 @app.get("/api/weather")
-async def get_weather(db: Session = Depends(get_db), limit: int = 10, page: int = 1):
+async def get_weather(db: Session = Depends(get_db), limit: int = 10, page: int = 1, year: int = 0, city: str = ""):
     skip = (page - 1) * limit
-    temperatures = db.query(Weather).limit(limit).offset(skip).all()
+    q = db.query(Weather)
+    if year != 0:
+        q = q.filter(Weather.date >= str(year)+'-01-01').filter(Weather.date <= str(year)+'-12-31')
+    if city != 0:
+        q = q.filter(Weather.city_code == city)
+    temperatures = q.limit(limit).offset(skip).all()
     return {'status': 'success', 'results': db.query(Weather).count(), 'weathers': temperatures }
 
 
 @app.get("/api/weather/stats")
-async def get_weather_stats(db: Session = Depends(get_db), limit: int = 10, page: int = 1):
+async def get_weather_stats(db: Session = Depends(get_db), limit: int = 10, page: int = 1, year: int = 0, city: str = ""):
     skip = (page - 1) * limit
-    stats = db.query(WeatherStats).limit(limit).offset(skip).all()
+    q = db.query(WeatherStats)
+    if year != 0:
+        q = q.filter(WeatherStats.year == year)
+    if city != 0:
+        q = q.filter(WeatherStats.city_code == city)
+    stats = q.limit(limit).offset(skip).all()
     return {'status': 'success', 'results': db.query(WeatherStats).count(), 'stats': stats }
